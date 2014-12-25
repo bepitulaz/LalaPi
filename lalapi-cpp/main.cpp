bbf5234990d075fdf45a0d51c6da41c3823d00c4
Asep Bagja Priandana <bepitulaz@gmail.com>
2014-12-25 12:56:09 +0700

    Initial Commit

---
+++ lalapi-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lalapi-cpp
+//
+//  Created by Asep Bagja on 12/25/14.
+//  Copyright (c) 2014 AsepCo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
